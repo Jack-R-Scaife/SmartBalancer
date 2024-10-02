de966c0c3513a165b2f1752f58fb1f2f08af27ab
Jack-R-Scaife <19008831@uhi.ac.uk>
2024-10-02 21:07:06 +0100

    Backend to Frontend Integration
    Changing Server page to dynamically show server information in the table.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify
 from app import db
+from app.models import Server, ServerGroup, ServerGroupServer  # Import necessary models
 from sqlalchemy import text
 
 # Blueprint definition
@@ -14,8 +15,32 @@ def index():
 
 # Web UI route for the server details page
 @main_blueprint.route('/server')
-def server():
-    return render_template('server.html')
+def servers():
+    # Query all servers
+    servers = Server.query.all()
+    
+    # Prepare data for the template
+    server_data = []
+    for server in servers:
+        # Get server group(s)
+        groups = [association.server_group.name for association in server.server_group_associations]
+        group_names = ', '.join(groups)
+        
+        # Get latest resource usage (Assuming you have a ResourceUsage model)
+        
+        server_info = {
+            'group': group_names ,
+            'status': server.status,
+            'name': getattr(server, 'name', 'N/A'),  # Use 'N/A' if name is not available
+            'ip_address': server.ip_address,
+            'cpu_usage': 'N/A',
+            'memory_usage':'N/A',
+            'disk_usage': 'N/A',
+            'network_traffic':'N/A',
+        }
+        server_data.append(server_info)
+    
+    return render_template('server.html', servers=server_data)
 
 
 # Web UI route for the server details page
