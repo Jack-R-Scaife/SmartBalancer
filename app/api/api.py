9ebe5cdad02e9a0dbd98e6889b67f0c03a09fc53
Jack-R-Scaife <19008831@uhi.ac.uk>
2024-11-06 18:06:05 +0000

    Updated API

--- app/api/api.py
+++ app/api/api.py
@@ -1,11 +1,19 @@
 # app/api/api.py
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
 from server.server_manager import ServerManager
 import requests
 from app.models import Server
+import json
 # Define the blueprint for API routes
 api_blueprint = Blueprint('api', __name__)
 
+status_mapping = {
+    "healthy": 1,
+    "overloaded": 2,
+    "critical": 3,
+    "down": 4,
+    "idle": 5
+}
 
 # Route to link a server
 @api_blueprint.route('/servers/link', methods=['POST'])
@@ -43,8 +51,8 @@ def link_server():
 # Route to get server status
 @api_blueprint.route('/server_status', methods=['GET'])
 def get_server_status():
-    # Query all the servers from the database
-    servers = Server.query.all()
+    servers = Server.query.all()  # Fetch all servers from the database
+    server_status_list = []
     
     for server in servers:
         numeric_status = status_mapping.get(server.status, 4)  # Default to "down" if status is not recognized
@@ -56,11 +64,10 @@ def get_server_status():
     response_data = json.dumps(server_status_list, separators=(',', ':'))  # Minify the response
     return Response(response_data, content_type='application/json')
 
+# api.py
 @api_blueprint.route('/servers/remove/<ip_address>', methods=['DELETE'])
 def remove_server(ip_address):
     server_manager = ServerManager()
-    # Use the remove_server method in ServerManager to handle the entire process
     result = server_manager.remove_server(ip_address)
-    # Set appropriate status codes based on whether the server was found and removed
     status_code = 200 if result['status'] else 404
     return jsonify({'message': result['message']}), status_code
