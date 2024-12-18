cc94ec311a75c7e4ea156e2ec9251760b8203269
Jack-R-Scaife <19008831@uhi.ac.uk>
2024-12-18 18:13:25 +0000

    Update uni_deploy.sh

--- uni_deploy.sh
+++ uni_deploy.sh
@@ -6,7 +6,7 @@ CLONE_DIR="/tmp/load_balancer_repo"
 AGENT_FOLDER="agent"
 BACKEND_VMS=("ubs1@192.168.1.3" "ubs2@192.168.1.4" "ubs3@192.168.1.5" "ubs4@192.168.1.6" "ubs5@192.168.1.7")
 LB_VM="192.168.1.2"
-TARGET_DIR="\$HOME/load_balancer"  # Directory to SCP files in the home directory
+TARGET_DIR="load_balancer"  # Subdirectory inside the user's home directory
 
 # Exit on error
 set -e
@@ -26,15 +26,15 @@ for VM in "${BACKEND_VMS[@]}"; do
 
     ssh "$VM" << EOF
         set -e
-        mkdir -p $TARGET_DIR/$AGENT_FOLDER
-        chmod -R 755 $TARGET_DIR
+        mkdir -p ~/$TARGET_DIR/$AGENT_FOLDER
+        chmod -R 755 ~/$TARGET_DIR
 EOF
 
-    scp -r "$CLONE_DIR/$AGENT_FOLDER" "$VM:$TARGET_DIR/"
+    scp -r "$CLONE_DIR/$AGENT_FOLDER" "$VM:~/$TARGET_DIR/"
 
     ssh "$VM" << EOF
         set -e
-        cd $TARGET_DIR/$AGENT_FOLDER
+        cd ~/$TARGET_DIR/$AGENT_FOLDER
         python3 -m venv venv
         source venv/bin/activate
         pip install --upgrade pip
@@ -48,8 +48,8 @@ EOF
 
         [Service]
         User=$(whoami)
-        WorkingDirectory=$TARGET_DIR/$AGENT_FOLDER
-        ExecStart=$TARGET_DIR/$AGENT_FOLDER/venv/bin/python $TARGET_DIR/$AGENT_FOLDER/agent.py
+        WorkingDirectory=~/$TARGET_DIR/$AGENT_FOLDER
+        ExecStart=~/$TARGET_DIR/$AGENT_FOLDER/venv/bin/python ~/$TARGET_DIR/$AGENT_FOLDER/agent.py
         Restart=always
 
         [Install]
@@ -64,12 +64,12 @@ done
 
 # Deploy to Load Balancer or REST VM
 echo "Deploying to Load Balancer/REST VM: $LB_VM"
-ssh "$LB_VM" "mkdir -p $TARGET_DIR && chmod -R 755 $TARGET_DIR"
-scp -r "$CLONE_DIR"/* "$LB_VM:$TARGET_DIR/"
+ssh "$LB_VM" "mkdir -p ~/$TARGET_DIR && chmod -R 755 ~/$TARGET_DIR"
+scp -r "$CLONE_DIR"/* "$LB_VM:~/$TARGET_DIR/"
 
 ssh "$LB_VM" << EOF
     set -e
-    cd $TARGET_DIR
+    cd ~/$TARGET_DIR
     python3 -m venv venv
     source venv/bin/activate
     pip install --upgrade pip
